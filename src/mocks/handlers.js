f366db965d6ed827598122657bf4b5806efac56d
manuelitox <manuel2788@gmail.com>
2020-10-13 18:09:49 +0200

    fix minor issue with the mocks url

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,4 +2,4 @@ import { rest } from "msw";
 
 import signup from "./signup";
 
-export const handlers = [rest.post("/signup", signup)];
+export const handlers = [rest.post("http://localhost:3000/signup", signup)];
